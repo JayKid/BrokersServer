5d07dc4b29fbc56be7802066ef80cd87ffcba57e
Jose E. Rodriguez <staff.tsr@gmail.com>
2013-09-09 02:19:42 +0200

    Modifying modules handling

--- .chef/knife.rb
+++
@@ -1,3 +0,0 @@
-current_dir = File.dirname(__FILE__)                                                                                                         
-chef_dir    = File.expand_path("#{current_dir}/../server")
-cookbook_path ["#{chef_dir}/cookbooks"]
