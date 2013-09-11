5491ae9643491ce67f98cd9a73a37943668eaf18
Jose E. Rodriguez <staff.tsr@gmail.com>
2013-09-11 22:40:46 +0200

    Preparing database in DB instance

--- roles/db.rb
+++ roles/db.rb
@@ -3,18 +3,20 @@ description "Systems that hosts the MySQL DB"
 run_list(
   "recipe[apt]",
   "recipe[mysql]",
-  "recipe[mysql::server]"
+  "recipe[mysql::server]",
+  "recipe[mysql::client]",
+  "recipe[api::db]"
 )
 
 default_attributes(
   "mysql" => {
-    "server_root_password" => "secret" #,
-#    "server_repl_password" => "secret",
-#    "server_debian_password" => "secret"
+    "server_root_password" => "secret",
+    "server_repl_password" => "secret",
+    "server_debian_password" => "secret"
   },
   "api" => {
      "database" => {
-        "name" => "market"
+        "name" => "market",
         "user" => "market",
         "password" => "themarket"
      }
